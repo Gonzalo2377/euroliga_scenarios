add78cc2a73f34eb1826abd26f77ed784fdc94b7
TBT <133041474+Gonzalo2377@users.noreply.github.com>
2026-04-25 13:30:44 +0200

    Update standings.js

--- function/api/standings.js
+++ function/api/standings.js
@@ -1,8 +1,6 @@
 export async function onRequest(context) {
   const url = new URL(context.request.url);
   const origin = context.request.headers.get('Origin') || '';
-
-  // Reenvía los mismos query params a la API real
   const apiUrl = `https://euroleague-advanced-api.eu/Euroleague/standings${url.search}`;
 
   const apiRes = await fetch(apiUrl, {
