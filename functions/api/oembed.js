c7c95e3759666c110df76d396c634344f2df9202
TBT <133041474+Gonzalo2377@users.noreply.github.com>
2026-04-29 09:58:50 +0200

    Change Twitter oEmbed theme from dark to light

--- functions/api/oembed.js
+++ functions/api/oembed.js
@@ -1,7 +1,7 @@
 export async function onRequest(context) {
     const tweetUrl = new URL(context.request.url).searchParams.get('url');
     const res = await fetch(
-        `https://publish.twitter.com/oembed?url=${encodeURIComponent(tweetUrl)}&theme=dark&dnt=true`
+        `https://publish.twitter.com/oembed?url=${encodeURIComponent(tweetUrl)}&theme=light&dnt=true`
     );
     const data = await res.json();
     return new Response(JSON.stringify(data), {
