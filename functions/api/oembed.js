cf008381458d89545b77064cf48d8fa321efbb24
TBT <133041474+Gonzalo2377@users.noreply.github.com>
2026-04-29 09:48:25 +0200

    Add oembed function to fetch Twitter embeds

---
+++ functions/api/oembed.js
@@ -0,0 +1,14 @@
+export async function onRequest(context) {
+    const tweetUrl = new URL(context.request.url).searchParams.get('url');
+    const res = await fetch(
+        `https://publish.twitter.com/oembed?url=${encodeURIComponent(tweetUrl)}&theme=dark&dnt=true`
+    );
+    const data = await res.json();
+    return new Response(JSON.stringify(data), {
+        headers: {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*',
+            'Cache-Control': 'public, max-age=3600'
+        }
+    });
+}
