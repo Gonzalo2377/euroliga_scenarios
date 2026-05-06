5b3737e7e334b9319331986a9f3a849f33a5c64f
TBT <133041474+Gonzalo2377@users.noreply.github.com>
2026-05-06 22:48:27 +0200

    Update oembed.js

--- functions/api/oembed.js
+++ functions/api/oembed.js
@@ -8,7 +8,7 @@ export async function onRequest(context) {
         headers: {
             'Content-Type': 'application/json',
             'Access-Control-Allow-Origin': '*',
-            'Cache-Control': 'public, max-age=3600'
+            'Cache-Control': 'public, max-age=86400'
         }
     });
 }
